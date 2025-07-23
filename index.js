a637e8fe6e971d931a915c227eb87df17e5c00a1
navin kumar <navinkumar02929@gmail.com>
2025-07-23 20:10:18 +0530

    issue_fix for route not found

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ const routes = require('./routes/index');
 
 
 app.use('/api',routes);
+app.use((req,res,next)=>{
+    res.status(404).json({
+        success: false,
+        message:"Route not found",
+        path: req.originalUrl
+    })
+})
+
 app.use('/',(req,res,next)=>{
     res.send('server is running')
 })
